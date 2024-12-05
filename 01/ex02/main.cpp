5914da9c9ea5a1b299ebff8000cbb5613ad7da30
Sergio Aires <sergioaires@gmail.com>
2024-12-05 16:30:59 +0100

    v1

--- 01/ex02/main.cpp
+++ 01/ex02/main.cpp
@@ -7,12 +7,12 @@ int    main()
     std::string* stringPTR = &str;
     std::string& stringREF = str;
 
-    std::cout << "Address: " << &str << std::endl;
-    std::cout << "Address: " << stringPTR << std::endl;
-    std::cout << "Address: " << &stringREF << std::endl;
+    std::cout << "Address of str: " << &str << std::endl;
+    std::cout << "Address stringPTR: " << stringPTR << std::endl;
+    std::cout << "Address stringREF: " << &stringREF << std::endl;
 
-    std::cout << "\nValue: " << str << std::endl;
-    std::cout << "Value: " << *stringPTR << std::endl;
-    std::cout << "Value: " << stringREF << std::endl;
+    std::cout << "\nValue of str: " << str << std::endl;
+    std::cout << "Value stringPTR: " << *stringPTR << std::endl;
+    std::cout << "Value stringREF: " << stringREF << std::endl;
     return 0;
 }
